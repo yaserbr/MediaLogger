6b3bf04cbda27d760e024b2ac765f984f65a253e
yaser8541 <443017396@sm.imamu.edu.sa>
2026-02-17 12:45:03 +0300

    Fix Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -3,43 +3,43 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "https://medialogger-6jne.onrender.com/auth/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ googleId: profile.id });
 
-        if (!user) {
-          user = await User.create({
-            googleId: profile.id,
-            username: profile.displayName,
-            email: profile.emails[0].value,
-          });
+                if (!user) {
+                    user = await User.create({
+                        googleId: profile.id,
+                        username: profile.displayName,
+                        email: profile.emails[0].value,
+                    });
+                }
+
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
+            }
         }
-
-        return done(null, user);
-      } catch (err) {
-        return done(err, null);
-      }
-    }
-  )
+    )
 );
 
 // حفظ المستخدم في السيشن
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 // جلبه من السيشن
 passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
